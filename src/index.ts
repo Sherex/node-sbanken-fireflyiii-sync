556f751ec645fcb619838becb8e47915b9d59364
Ingar Helgesen <ingar@i-h.no>
2020-07-18 08:08:43 +0200

    Using @sherex/sbanken instead

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import SBanken from './lib/sbanken'
-import { config } from './lib/load-config'
+import { SBanken } from '@sherex/sbanken'
+import { sbanken } from './lib/load-config'
 
 (async () => {
-  const client = new SBanken(config)
-  console.log(JSON.stringify(await client.getToken(), null, 2))
+  const client = new SBanken(sbanken)
   console.log(JSON.stringify(await client.getAccounts(), null, 2))
 })().catch(console.error)
--- src/lib/to-url-encoded-form-data.ts
+++
@@ -1,9 +0,0 @@
-interface FormDataObject {
-  [key: string]: string
-}
-
-export default function toUrlEncodedFormData (obj: FormDataObject): URLSearchParams {
-  const params = new URLSearchParams()
-  Object.entries(obj).forEach(([key, value]) => params.append(key, value))
-  return params
-}
